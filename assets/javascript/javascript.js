b6a0abac7f2028921009942665ce71f44ea7bc3d
Matt <n.matt30@yahoo.com>
2017-09-10 13:48:15 -0700

    first commit

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+    var sports = ["volleyball", "football", "basketball", "baseball", "soccer"];
+    var search = "";
+    for(var i = 0; i < sports.length; i++) {
+        var newButton = $("<button>");
+        newButton.text(sports[i])
+            .attr("id", sports[i])
+            .on("click", function() {
+            search = this.id;
+            var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ search +"&api_key=198ada0e52ad45b6ac0db3bb03bd0649&limit=10";
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).done(function(response) {
+                for(var x = 0; x < response.data.length; x++) {
+                    console.log(response.data[x].url);
+                    var image = $("<img>");
+                    image.attr("src", response.data[x].images.fixed_height_still.url)
+                        .on("click", function () {
+                            image.attr("src", response.data[x].images.fixed_height.url);
+                        });
+                    $("#sports").append(image);
+                }
+            });
+        });
+        $("#sports-buttons").append(newButton);
+    };
+});
