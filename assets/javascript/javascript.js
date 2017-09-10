108a34cd24914ac6157617fb034fb8cb6e4e8927
Matt <n.matt30@yahoo.com>
2017-09-10 15:11:00 -0700

    WIP

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -17,13 +17,31 @@ $(document).ready(function() {
                     console.log(response.data[x].url);
                     var image = $("<img>");
                     image.attr("src", response.data[x].images.fixed_height_still.url)
+                        .attr("id", search)
+                        .attr("index", x)
                         .on("click", function () {
-                            image.attr("src", response.data[x].images.fixed_height.url);
+                            // image.attr("src", response.data[x].images.fixed_height.url);
+                            animateGif(image);
                         });
                     $("#sports").append(image);
                 }
             });
         });
         $("#sports-buttons").append(newButton);
+    };
+
+    function animateGif(image) {
+        search = image.attr("id");
+        index = image.attr("index");
+        console.log(search);
+        console.log(image.attr("index"));
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ search +"&api_key=198ada0e52ad45b6ac0db3bb03bd0649&limit=10";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function(response) {
+            image.attr("src", response.data[index].images.fixed_height.url);
+            $("#sports").append(image);
+        });
     };
 });
